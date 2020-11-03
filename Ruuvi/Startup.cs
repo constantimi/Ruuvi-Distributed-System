7a667ed7144a667ec75c320162b83c163a59e507
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-11-03 15:56:54 +0100

    Update Startup.cs

--- Ruuvi/Startup.cs
+++ Ruuvi/Startup.cs
@@ -16,14 +16,12 @@ namespace Ruuvi
 {
     public class Startup
     {
-        private IConfiguration _configuration;
-
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get => _configuration; set => _configuration = value; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
